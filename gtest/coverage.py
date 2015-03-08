f9f84841f6a9210b621884b6631142bf753c0d20
Michael Wayne Goodman <goodman.m.w@gmail.com>
2015-03-08 16:47:28 +0800

    Coverage test works for parses.

--- gtest/coverage.py
+++ gtest/coverage.py
@@ -1,10 +1,12 @@
 
+from collections import OrderedDict
 from functools import partial
+from os.path import join as pjoin, basename
 
 from gtest.util import (
     prepare_working_directory, prepare_compiled_grammar,
     debug, info, warning, error, red, green, yellow,
-    check_exist, make_keypath,
+    check_exist, make_keypath, dir_is_profile,
     mkprof, run_art
 )
 
@@ -52,15 +54,14 @@ def coverage_test(args):
         if not (check_exist(skel.path)):
             print(skip_msg)
             continue
-        
+
         with open(logf, 'w') as logfile:
             mkprof(skel.path, dest, log=logfile)
             run_art(
                 args.compiled_grammar.path,
                 dest,
                 log=logfile)
-            pc = parsing_coverage(dest)
-            print(format_coverage_summary(pc))
+            cov = parsing_coverage(dest)
 
             if args.generate:
                 g_dest = pjoin(args.working_dir, basename(skel.path) + '.g')
@@ -72,17 +73,54 @@ def coverage_test(args):
                     ace_options=['-e'],
                     log=logfile
                 )
-                gc = generation_coverage(g_dest, pc)
-                print(format_coverage_summary(gc))
+                cov = generation_coverage(g_dest, cov)
+            print_coverage_summary(name, cov)
 
 
-def parsing_coverage(prof):
-    pass
+def parsing_coverage(prof_path):
+    cov = OrderedDict([
+        ('items', 0),
+        ('has_parse', 0),
+        ('readings', 0),
+    ])
+    prof = itsdb.ItsdbProfile(prof_path)
+    for row in prof.read_table('parse'):
+        cov['items'] += 1
+        readings = int(row['readings'])
+        if readings > 0:
+            cov['has_parse'] += 1
+            cov['readings'] += readings
+    return cov
 
 
-def generation_coverage(prof, pc):
-    pass
+def generation_coverage(prof_path, pc):
+    cov = dict(pc)
+    return cov
 
 
-def format_coverage_summary(coverage):
-    pass
+def print_coverage_summary(name, coverage):
+    print('{}:'.format(name))
+    div = 0
+    if coverage['items']:
+        div = float(coverage['has_parse']) / coverage['items']
+    print('  parses  : {}/{} ({})'.format(
+        coverage['has_parse'], coverage['items'],
+        color('{:0.4g}', div, PARSE_OK, PARSE_GOOD)
+    ))
+    div = 0
+    if coverage['has_parse']:
+        div = float(coverage['readings']) / coverage['has_parse']
+    print('  readings: {}/{} ({:0.4g} per parsed item)'.format(
+        coverage['readings'], coverage['has_parse'], div
+    ))
+
+
+def color(fmt, x, ok_thresh, good_thresh):
+    s = fmt.format(x)
+    if x >= ok_thresh:
+        if x >= good_thresh:
+            return green(s)
+        else:
+            return yellow(s)
+    else:
+        return red(s)
