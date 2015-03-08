c261faf36fc5b110930b70cd7f07bf35b4f1557c
Michael Wayne Goodman <goodman.m.w@gmail.com>
2015-03-08 23:34:37 +0800

    Fix coverage test for i-wf differences.

--- gtest/coverage.py
+++ gtest/coverage.py
@@ -78,18 +78,32 @@ def coverage_test(args):
 
 
 def parsing_coverage(prof_path):
+    # todo: consider i-wf
     cov = OrderedDict([
-        ('items', 0),
+        ('items', 0), # items with i-wf = 1
+        ('*items', 0), # items with i-wf = 0
+        ('?items', 0), # items with i-wf = 2
         ('has_parse', 0),
+        ('*has_parse', 0),
         ('readings', 0),
+        ('*readings', 0)
     ])
     prof = itsdb.ItsdbProfile(prof_path)
-    for row in prof.read_table('parse'):
-        cov['items'] += 1
-        readings = int(row['readings'])
-        if readings > 0:
-            cov['has_parse'] += 1
+    for row in prof.join('item', 'parse'):
+        wf = int(row['item:i-wf'])
+        readings = int(row['parse:readings'])
+        if wf == 0:
+            cov['*items'] += 1
+            if readings > 0:
+                cov['*has_parse'] += 1
+            cov['*readings'] += readings
+        elif wf == 1:
+            cov['items'] += 1
+            if readings > 0:
+                cov['has_parse'] += 1
             cov['readings'] += readings
+        else:
+            cov['?items'] += 1
     return cov
 
 
@@ -98,25 +112,56 @@ def generation_coverage(prof_path, pc):
     return cov
 
 
-def print_coverage_summary(name, coverage):
-    print('{}:'.format(name))
-    div = 0
-    if coverage['items']:
-        div = float(coverage['has_parse']) / coverage['items']
-    print('  parses  : {}/{} ({})'.format(
-        coverage['has_parse'], coverage['items'],
-        color('{:0.4g}', div, PARSE_OK, PARSE_GOOD)
+template2 = '  {:12s}: {:4d}/{:<4d} ({:6.4f}) : {:4d}/{:<4d} ({:6.4f})'
+template2s = '  {:12s}: {:4d}/{:<4d} ({:6s}) : {:4d}/{:<4d} ({:6s})'
+template3 = '  {:12s}: {:4d}/{:<4d} ({:6.4f}) : {:4d}/{:<4d} ({:6.4f}) : {:4d}/{:<4d} ({:6.4f}))'
+
+def print_coverage_summary(name, cov):
+    # todo: fix i-wf calculations for parsing
+    item_total = cov['items'] + cov['*items'] + cov['?items']
+    print('{} ({} items):'.format(name, item_total))
+    if item_total == 0:
+        print('  No items.')
+        return
+    print('              : grammatical        : ungrammatical      : ignored ')
+    print(template3.format(
+        'items',
+        cov['items'], item_total, float(cov['items']) / item_total,
+        cov['*items'], item_total, float(cov['*items']) / item_total,
+        cov['?items'], item_total, float(cov['?items']) / item_total
     ))
-    div = 0
-    if coverage['has_parse']:
-        div = float(coverage['readings']) / coverage['has_parse']
-    print('  readings: {}/{} ({:0.4g} per parsed item)'.format(
-        coverage['readings'], coverage['has_parse'], div
+    val1 = val2 = 0
+    if cov['items']:
+        val1 = float(cov['has_parse']) / cov['items']
+    if cov['*items']:
+        val2 = float(cov['*has_parse']) / cov['*items']
+    #if cov['*items']:
+
+    print(template2s.format(
+        'parses',
+        cov['has_parse'], cov['items'],
+        color('{:6.4f}', val1, PARSE_OK, PARSE_GOOD),
+        cov['*has_parse'], cov['*items'],
+        color('{:6.4f}', val2, PARSE_OK, PARSE_GOOD, invert=True)
     ))
+    val1 = val2 = 0
+    if cov['has_parse']:
+        val1 = float(cov['readings']) / cov['has_parse']
+    if cov['*has_parse']:
+        val2 = float(cov['*readings']) / cov['*has_parse']
+    print(template2.format(
+        'readings',
+        cov['readings'], cov['has_parse'], val1,
+        cov['*readings'], cov['*has_parse'], val2
+    ))
+    #print('  realizations:')
+    print()
 
 
-def color(fmt, x, ok_thresh, good_thresh):
+def color(fmt, x, ok_thresh, good_thresh, invert=False):
     s = fmt.format(x)
+    if invert:
+        x = 1 - x
     if x >= ok_thresh:
         if x >= good_thresh:
             return green(s)
