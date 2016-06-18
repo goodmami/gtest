d1713b712ecf3f6af908823e7044cf69a61bb4f4
Michael Wayne Goodman <goodman.m.w@gmail.com>
2016-06-17 17:57:44 -0700

    Add metadata and CHANGELOG.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Change Log
+
+## [Unreleased][unreleased]
+
+## [v0.1.0][]
+
+First versioned release. For previous changes, please consult the
+[commit history](../../commits/master).
+
+[unreleased]: ../../tree/develop
+[v0.1.0]: ../../releases/tag/v0.1.0
+[README]: README.md
---
+++ gtest/__about__.py
@@ -0,0 +1,17 @@
+
+# This __about__.py file for storing project metadata is inspired by
+# the warehouse project:
+#   https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+
+__version__ = '0.1.0'
+__version_info__ = __version__.replace('.', ' ').replace('-', ' ').split()
+
+__title__ = 'gTest'
+__summary__ = 'Command-line tool for grammar testing'
+__uri__ = 'https://github.com/goodmami/gtest'
+
+__author__ = 'Michael Wayne Goodman'
+__email__ = 'goodman.m.w@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = '2014 %s <%s> and contributors' % (__author__, __email__)
